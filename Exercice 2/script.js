91f996339800bf724a62b266df62ec7d230f620d
Mohamed Sahnoun <mohamed.sahnoun.professional>
2021-03-13 14:43:06 +0100

    TP1

--- Exercice 2/script.js
+++ Exercice 2/script.js
@@ -1,5 +1,12 @@
-    let colorInput = document.querySelector("#col");
-    let content = document.querySelector("#text");
+let colorInput = document.querySelector("#col");
+let content = document.querySelector("#text");
+
+document.getElementById('mySelect').addEventListener('change', function(){
+    content.style.fontFamily = this.value;
+  });
+    
+    
+    
      colorInput.addEventListener("change", () => {
       let color = colorInput.value;
       content.style.color = color;
