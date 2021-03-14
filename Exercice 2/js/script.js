df37aa065a96aff94d4e742f3a2e8a10a17d665c
Mohamed Sahnoun <mohamed.sahnoun.professional>
2021-03-14 19:02:13 +0100

    TP 1

---
+++ Exercice 2/js/script.js
@@ -0,0 +1,14 @@
+let content = document.querySelector("#text");
+
+document.getElementById('police').addEventListener('change', function(){
+    content.style.fontSize = this.value +"px";
+});
+
+document.getElementById('mySelect').addEventListener('change', function(){
+  content.style.fontFamily = this.value;
+});
+    
+    
+document.getElementById("col").addEventListener('change', function(){
+      content.style.color = this.value;
+});
